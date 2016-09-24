7feb26544a57755daf34c514328444007799b0b2
Albert.Zhou <albert748@gmail.com>
2016-09-24 21:08:48 +0800

    fix ggtags backend for parsing elisp
    ggtags use "ctags" as default backend which do not support identifier references.
    change to use "pygments" instead of "ctags".
    Make sure you've installed python and python-pygments from package manager.

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -1,3 +1,5 @@
+;; @see https://github.com/leoliu/ggtags
+
 (push 'ggtags melpa-include-packages)
 (require-package 'ggtags)
 
@@ -9,6 +11,10 @@
         ;; FIX: remove default key binding for `evil-repeat-pop-next' from evil-mode if exist
         (define-key evil-normal-state-map (kbd "M-.") nil))
     (when (executable-find (ggtags-program-path "global"))
+      ;; FIX: ggtags use "ctags" as default backend which do not support identifier references.
+      ;; gtags builtin lables include: default, native, user, ctags, new-ctags, pygments
+      ;; Make sure you've installed python and python-pygments from package manager.
+      (setenv "GTAGSLABEL" "pygments")
       (ggtags-mode))))
 
 (add-hook 'emacs-lisp-mode-hook 'ggtags-mode-init)
