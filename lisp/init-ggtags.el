85f5d398180ba096e39c9e8183999e68e30841f7
Albert.Zhou <albert748@gmail.com>
2016-09-24 18:59:09 +0800

    Fix conflict key binding for ggtags
    remove default key binding for `evil-repeat-pop-next' from evil-mode if exist

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -3,7 +3,11 @@
 
 ;; lisp setup
 (defun ggtags-mode-init ()
+  "setup ggtags mode for available modes."
   (with-eval-after-load 'ggtags
+    (if evil-normal-state-map
+        ;; FIX: remove default key binding for `evil-repeat-pop-next' from evil-mode if exist
+        (define-key evil-normal-state-map (kbd "M-.") nil))
     (when (executable-find (ggtags-program-path "global"))
       (ggtags-mode))))
 
