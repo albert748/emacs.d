66eb432c94016015ead39f53086d6e041d490894
Albert.Zhou <albert748@gmail.com>
2016-10-07 12:39:14 +0800

    Fix ggtags-mode load issue

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -6,13 +6,15 @@
   (with-eval-after-load 'ggtags
     (if evil-normal-state-map
         ;; FIX: remove default key binding for `evil-repeat-pop-next' from evil-mode if exist
-        (define-key evil-normal-state-map (kbd "M-.") nil))
-    (when (executable-find (ggtags-program-path "global"))
-      ;; FIX: ggtags use "ctags" as default backend which do not support identifier references.
-      ;; gtags builtin lables include: default, native, user, ctags, new-ctags, pygments
-      ;; Make sure you've installed python and python-pygments from package manager.
-      (setenv "GTAGSLABEL" "pygments")
-      (ggtags-mode))))
+        (define-key evil-normal-state-map (kbd "M-.") nil)))
+
+  ;; Enable ggtags only if executable found.
+  (when (executable-find "global")
+    ;; FIX: ggtags use "ctags" as default backend which do not support identifier references.
+    ;; gtags builtin lables include: default, native, user, ctags, new-ctags, pygments
+    ;; Make sure you've installed python and python-pygments from package manager.
+    (setenv "GTAGSLABEL" "pygments")
+    (ggtags-mode)))
 
 (add-hook 'emacs-lisp-mode-hook 'ggtags-mode-init)
 
