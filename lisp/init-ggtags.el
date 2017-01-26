7d8bf6bbeb68bef015b274ee6f117eda28dee806
Albert.Zhou <albert748@gmail.com>
2017-01-27 04:13:23 +0800

    Disable ggtags under windows

--- lisp/init-ggtags.el
+++ lisp/init-ggtags.el
@@ -1,29 +1,30 @@
 ;; @see https://github.com/leoliu/ggtags
 
-;; lisp setup
-(defun ggtags-elisp-mode-hook-setup ()
-  "setup ggtags for elisp mode."
-  (ggtags-mode)
+(use-package ggtags
+  ;; DO NOT enable gtags as global is not usable under windows from msys2
+  :if (not (eq system-type 'windows-nt))
 
-  (if evil-normal-state-map
-      ;; FIX: remove default key binding for `evil-repeat-pop-next' from evil-mode if exist
-      (define-key evil-normal-state-map (kbd "M-.") nil))
+  :init
+  (unless (executable-find "gtags")
+    (message "Your need install global, ctags, pygments to enable ggtags"))
 
-  (with-eval-after-load 'ggtags
+  :config
+  (defun ggtags-elisp-mode-hook-setup ()
+    "setup ggtags for elisp mode."
+    (ggtags-mode)
+
+    (if evil-normal-state-map
+        ;; FIX: remove default key binding for `evil-repeat-pop-next' from evil-mode if exist
+        (define-key evil-normal-state-map (kbd "M-.") nil))
+
     ;; GTAGSLOGGING: Path name to the log file
     ;; FIX: ggtags use "ctags" as default backend which do not support identifier references.
     ;; gtags builtin lables include: default, native, user, ctags, new-ctags, pygments
     ;; Make sure you've installed python and python-pygments from package manager.
-    (setq-local ggtags-process-environment (list "GTAGSLABEL=pygments"))))
+    (setq-local ggtags-process-environment (list "GTAGSLABEL=pygments")))
 
 
-(defun my-ggtags-enable ()
-  ;; Enable ggtags only if executable found.
-  (if (not (executable-find "gtags"))
-      (message "Your need install global, ctags, pygments to enable ggtags")
-    (add-hook 'emacs-lisp-mode-hook 'ggtags-elisp-mode-hook-setup)))
-
-(my-ggtags-enable)
+  (add-hook 'emacs-lisp-mode-hook 'ggtags-elisp-mode-hook-setup))
 
 
 (provide 'init-ggtags)
