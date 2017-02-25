4c280cb1682c7e22134273189e8a413723c597ad
Albert.Zhou <albert748@gmail.com>
2017-02-25 17:53:32 +0800

    load org-babel languages on-demand

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -199,6 +199,14 @@ If use-indirect-buffer is not nil, use `indirect-buffer' to hold the widen conte
   ;; :pin org
 
   :config
+  (defadvice org-babel-execute-src-block (around load-language nil activate)
+    "Load language on-demand"
+    (let ((language (org-element-property :language (org-element-at-point))))
+      (unless (cdr (assoc (intern language) org-babel-load-languages))
+        (add-to-list 'org-babel-load-languages (cons (intern language) t))
+        (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages))
+      ad-do-it))
+
   ;; org mode use truncate-lines as default, reset it to nil.
   ;; we do not want to use word-wrap, beacause it's ugly for chinese.
   ;; FIXME: word-wrap chinese only.
