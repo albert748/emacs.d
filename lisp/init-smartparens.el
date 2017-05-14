0650a03ea8603a995762fea96255ac75809f423c
Albert.Zhou <albert748@gmail.com>
2017-05-14 08:52:44 +0800

    add sp-wrap-with-parens

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -6,14 +6,23 @@
 ;; expected.
 (use-package smartparens
   :init (require 'smartparens-config)
+  :bind* (:map smartparens-mode-map
+              ("M-(" . sp-wrap-with-parens))
   :config
+
+  (defun sp-wrap-with-parens ()
+    "use sp-wrap-with-pair for sexp parenthese wrap around.
+useful feature found in paredit but missing from smartparens"
+    (interactive)
+    (sp-wrap-with-pair "("))
+
+  ;; enable additional feature
   (add-hook 'smartparens-enabled-hook #'show-smartparens-mode)
 
   ;; set default set of key bindings: paredit or smartparens style
   ;; (sp-use-paredit-bindings)
   (sp-use-smartparens-bindings)
 
-  ;; (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
   (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
   (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
 
