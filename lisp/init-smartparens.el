9876e3f346c35588c87675a50c4aa3fc01a5c443
Albert.Zhou <albert748@gmail.com>
2018-01-16 15:55:57 +0800

    Update smartparens

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,33 +1,43 @@
-;; @see https://github.com/Fuco1/smartparens
+;;; init-smartparens.el --- see https://github.com/Fuco1/smartparens
+
+;;; Commentary:
+
 ;; @see https://ebzzry.github.io/emacs-pairs.html#wrapping
 
+;;; Code:
+
 ;; To enable smartparens on evil visual state, you'd enable (setq
-;; evil-move-beyond-eol t), otherwise sp-forward-sexp may not work as
+;; evil-move-beyond-eol t, otherwise sp-forward-sexp may not work as
 ;; expected.
 (use-package smartparens
-  :init (require 'smartparens-config)
-  :bind* (:map smartparens-mode-map
-              ("M-(" . sp-wrap-with-parens))
+  :bind (:map smartparens-mode-map ("M-(" . sp-wrap-with-parentheses))
+
+  :init
+  (require 'smartparens-config)
+  (smartparens-global-mode)
+
   :config
+  ;; because `evil-smartparens-mode' default is strict, do not add it
+  ;; to `smartparens-enabled-hook'
+  (use-package evil-smartparens)
 
-  (defun sp-wrap-with-parens ()
-    "use sp-wrap-with-pair for sexp parenthese wrap around.
+  (sp-use-smartparens-bindings)         ; or (sp-use-paredit-bindings)
+
+  ;; enable additional feature which is not enabled by default
+  (add-hook 'smartparens-enabled-hook #'turn-on-show-smartparens-mode)
+
+  (defun smartparens-strict-mode-setup ()
+    (evil-smartparens-mode)
+    (turn-on-smartparens-strict-mode))
+
+  ;; FIXME: trun-on trict mode for each of sp-lisp-modes
+  (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode-setup)
+
+  (defun sp-wrap-with-parentheses (&optional arg)
+    "use `sp-wrap-with-pair' to enclose the sexp next around.
 useful feature found in paredit but missing from smartparens"
-    (interactive)
-    (sp-wrap-with-pair "("))
-
-  ;; enable additional feature
-  (add-hook 'smartparens-enabled-hook #'show-smartparens-mode)
-
-  ;; set default set of key bindings: paredit or smartparens style
-  ;; (sp-use-paredit-bindings)
-  (sp-use-smartparens-bindings)
-
-  (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-  (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
-
-  (use-package evil-smartparens
-    :config
-    (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)))
+    (interactive "P")
+    (sp-wrap-with-pair "(")))
 
 (provide 'init-smartparens)
+;;; init-smartparens.el ends here
