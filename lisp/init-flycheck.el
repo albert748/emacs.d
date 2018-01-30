295769663918340cae4f18f76c29893fa5ab2b16
Albert.Zhou <albert748@gmail.com>
2018-01-30 09:59:13 +0800

    Disable checkdoc on flycheck mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,5 +1,17 @@
+;;; init-flycheck.el --- see http://www.flycheck.org/
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package flycheck
-  :init (global-flycheck-mode))
+  :init (global-flycheck-mode)
+
+  :config
+  (defun flycheck-mode-org-setup ()
+    (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+  (add-hook 'org-src-mode-hook #'flycheck-mode-org-setup))
+
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
