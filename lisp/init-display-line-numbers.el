61cca4744c25f9b6afdc3150160b94fd4f80ec79
Albert.Zhou <albert748@gmail.com>
2018-09-04 17:46:08 +0800

    Update

--- lisp/init-display-line-numbers.el
+++ lisp/init-display-line-numbers.el
@@ -8,7 +8,11 @@
   :ensure nil
   :init
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-  (add-hook 'org-mode-hook #'display-line-numbers-mode))
+  (add-hook 'org-mode-hook #'display-line-numbers-mode)
+
+  :config
+  ;; what's the difference between 'relative and 'visual?
+  (setq display-line-numbers-type 'visual))
 
 (provide 'init-display-line-numbers)
 ;;; init-display-line-numbers ends here
