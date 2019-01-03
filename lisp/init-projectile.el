cfca94f845f67b7989fdbca9853ea3ad43bd51f3
Albert.Zhou <albert748@gmail.com>
2019-01-03 10:25:01 +0800

    Move projectile saved files to cache directory

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+(eval-when-compile (defvar my-emacs-cache-directory))
+
 (use-package projectile
   :init
   (defun my-projectile-enable ()
@@ -11,7 +13,12 @@
     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
   (add-hook 'prog-mode-hook #'my-projectile-enable)
-  (add-hook 'org-mode-hook #'my-projectile-enable))
+  (add-hook 'org-mode-hook #'my-projectile-enable)
+
+  :config
+  (setq projectile-cache-file (expand-file-name "projectile.cache" my-emacs-cache-directory))
+  (setq projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" my-emacs-cache-directory)))
+
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
